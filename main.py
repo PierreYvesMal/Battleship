47e16d40098c999e3c2cf3b31da9414612b7de1f
Louisrem <115938054+Louisrem@users.noreply.github.com>
2026-02-18 12:03:01 +0100

    Questions sur les fonctions 1 et 2 : 1. Sur create_grid : J'ai galéré à bien séparer les listes internes (tout se mélangeait dans une seule liste au début). 2. Sur print_grid : Comment tester si le rendu est correct ? J'ai essayé des choses à côté mais je ne suis pas sûr. 3. Sur le rendu final : Si je fais juste ' '.join(line), comme la grille est vide au début, est-ce que l'affichage sera invisible ? Pourquoi ne pas faire un simple print(line) ? 4. Sur len() : Je ne vois pas bien où l'utiliser ici, à part pour compter les rangées/colonnes.

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ def create_grid(m: int, n: int) -> list[list[str]]:
     :param n: Number of columns
     :returns: A 2D list representing an M x N grid
     """
-    pass
+    grid = []
+    for l in range(m):
+        line = []
+        for c in range(n):
+            line.append(' ')
+        grid.append(line)
+    for line in grid:
+        print(''.join(line),end='')
+
+create_grid(5,5)
 
 def print_grid(grid: list[list[str]]) -> None:
+    for line in grid:
+        print(''.join(line),end='')
+
     """
     Prints the grid to the console.
     :param grid: A 2D list representing the grid
     """
+    ''
     # Hint: check the join method for strings and the len function for lists
     # Hint: check the end parameter of the print function to avoid printing a newline character
     pass
---
+++ test.py
@@ -0,0 +1,12 @@
+
+
+
+
+grid = []
+for l in range(5):
+    line = []
+    for c in range(5):
+        line.append(' ')
+    grid.append(line)
+print(grid)
+
